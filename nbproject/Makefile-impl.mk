240b7b611647172c68afd8fe8b0d108bbe22dd3b
Andrew Sentman <asentman.s6@gmail.com>
2018-05-07 18:26:34 -0700

    Updated MPLAB path

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=GlobalCurrent.X
+PROJECTNAME=GlobalCircuit
 
 # Active Configuration
 DEFAULTCONF=default
--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/GlobalCurrent.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=GlobalCurrent.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=globalcurrent.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/GlobalCircuit.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=GlobalCircuit.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=globalcircuit/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/globalcurrent.x/bin
+makeDirectory ${TMPDIR}/globalcircuit/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/globalcurrent.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/globalcircuit.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/globalcurrent.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/globalcircuit.tar *
 checkReturnCode
 
 # Cleanup
