74069e412d919388e2b975d8fff5ea05ad58f9e5
Andrew Thomas Sentman <andrew.sentman@dit2979us.DIGIPEN.EDU>
2018-05-18 15:20:31 -0700

    decode_packet outputs location in decimal degree

--- decode_packet.py
+++ decode_packet.py
@@ -17,14 +17,22 @@ def decb64(b64):
 			val+=cval*mul
 			mul*=64
 		return val
+
+def dm2d(i):
+  i/=1000000.0
+  d=int(i)
+  m=(i-d)*100
+  return d+m/60.0
+
 while True:
-	pack=raw_input("Packet:")
-	bTime=pack[:9]
-	bLati=pack[9:18]
-	bLong=pack[18:28]
-	bAlti=pack[28:33]
-	print bTime+':'+bLati+':'+bLong+':'+bAlti+':'+pack[33:]
-	print 'Time:%d(%s)'%(decb64(bTime),bTime.lstrip())
-	print 'Lati:%d(%s)'%(decb64(bLati),bLati.lstrip())
-	print 'Long:%d(%s)'%(decb64(bLong),bLong.lstrip())
-	print 'Alti:%d(%s)'%(decb64(bAlti),bAlti.lstrip())
+  pack=raw_input("Packet:")
+  bTime=pack[:9]
+  bLati=pack[9:18]
+  bLong=pack[18:28]
+  bAlti=pack[28:33]
+  print bTime+':'+bLati+':'+bLong+':'+bAlti+':'+pack[33:]
+  print 'Time:%d(%s)'%(decb64(bTime),bTime.lstrip())
+  print 'Lati:%f(%s)'%(dm2d(decb64(bLati)),bLati.lstrip())
+  print 'Long:%f(%s)'%(dm2d(decb64(bLong)),bLong.lstrip())
+  print 'Alti:%f(%s)'%(decb64(bAlti)/10.0,bAlti.lstrip())
+  print 'Location:%f, -%f'%(dm2d(decb64(bLati)),dm2d(decb64(bLong)))
