7ff4a7f806d258a8244965bc9eb472d21da5aa2e
Andrew Sentman <asentman.s6@gmail.com>
2018-06-19 13:57:32 -0700

    GPIO debugging working

--- GPIODebug.c
+++ GPIODebug.c
@@ -7,13 +7,11 @@ void IOInit() {
 }
 void IOWriteChar(char c) {
     int i;
-    PORTECLR=0x1FF;
-    PORTESET=0xFF&c;
-    for (i=100;i;i--);
-    PORTESET=0x100;
+    PORTE=PORTE&0xFEFF;
+    PORTE=(PORTE&0xFF00)|c;
+    PORTE=PORTE|0x100;
 }
 void IOWriteString(char *s) {
-    while (s) {
-        IOWriteChar(*(s++));
-    }
+    for (;*s;s++)
+        IOWriteChar(*s);
 }
